6cd7e10414f0955d36717b1b870109a384252418
Vadim Roschupkin <vroshupkin1@gmail.com>
2024-06-23 04:40:34 +0300

    Добавлен логгер

--- src/common/common.ts
+++ src/common/common.ts
@@ -1,4 +1,40 @@
 import {ENV} from '../env'
+import * as colors from 'colors'
 
 export const URL = `http://localhost:${ENV.PORT}`;
 
+type TLoggerOptions = {
+	withFileLog?: true,
+	disable?: true
+}
+
+// eslint-disable-next-line 
+const logger_constructor = (id: number, options?: TLoggerOptions) => (str: string | number | Record<any, any>) => 
+{
+	if(typeof str === 'object' && str !== null)
+	{
+		str = JSON.stringify(str);
+	}
+
+	const HEADER = colors.bgGreen(` LOGGER ${id} `);
+	
+	console.log(`${HEADER} ${str}`)  
+
+	if(!options)
+	{
+		return;
+	}
+
+	if(options.withFileLog)
+	{
+		console.log(getFileLog())
+	}
+}
+
+const getFileLog = () => 
+{
+	const err = new Error();
+	console.log(err.stack.split('\n')[3]);
+}
+	
+export const logger_1 = logger_constructor(1, {withFileLog: true});
